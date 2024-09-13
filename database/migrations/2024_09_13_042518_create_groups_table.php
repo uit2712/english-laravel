033ebb192d09590c2c191d3b9b5159074693df44
uit2712 <phamquangvi456@gmail.com>
2024-09-13 04:28:21 +0000

    add GroupSeeder

--- database/migrations/2024_09_13_042518_create_groups_table.php
+++ database/migrations/2024_09_13_042518_create_groups_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('groups');
     }
 };
---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Core\Features\Group\Constants\GroupConstants;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table(GroupConstants::TABLE_NAME)->insert([
+            'name' => 'Động vật',
+        ]);
+    }
+}
