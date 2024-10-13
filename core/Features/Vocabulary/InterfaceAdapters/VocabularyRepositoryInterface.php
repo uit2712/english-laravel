a54394818537e4ab12f834e70cfeb2d0270ba591
uit2712 <phamquangvi456@gmail.com>
2024-10-13 15:46:11 +0000

    update VocabularyRepository: add getByTopicId

--- core/Features/Vocabulary/InterfaceAdapters/VocabularyRepositoryInterface.php
+++ core/Features/Vocabulary/InterfaceAdapters/VocabularyRepositoryInterface.php
@@ -2,12 +2,20 @@
 
 namespace Core\Features\Vocabulary\InterfaceAdapters;
 
+use Core\Features\Vocabulary\Models\GetListVocabulariesResult;
 use Core\Features\Vocabulary\Models\GetVocabularyResult;
 use Core\Models\Result;
 
 interface VocabularyRepositoryInterface
 {
     public function isTableExisted(): bool;
+    /**
+     * @param int|null $id Id.
+     */
     public function get($id): GetVocabularyResult;
     public function reset(): Result;
+    /**
+     * @param int|null $topicId Topic id.
+     */
+    public function getByTopicId($topicId): GetListVocabulariesResult;
 }
--- core/Features/Vocabulary/Repositories/VocabularyRepository.php
+++ core/Features/Vocabulary/Repositories/VocabularyRepository.php
@@ -9,7 +9,9 @@ use Core\Features\Database\Facades\Database;
 use Core\Features\Vocabulary\Constants\VocabularyConstants;
 use Core\Features\Vocabulary\Facades\Vocabulary;
 use Core\Features\Vocabulary\InterfaceAdapters\VocabularyRepositoryInterface;
+use Core\Features\Vocabulary\Models\GetListVocabulariesResult;
 use Core\Features\Vocabulary\Models\GetVocabularyResult;
+use Core\Helpers\ArrayHelper;
 use Core\Helpers\NumberHelper;
 use Core\Models\Result;
 
@@ -55,6 +57,29 @@ class VocabularyRepository implements VocabularyRepositoryInterface
     {
         $result = new Result();
         Database::truncate($this->tableName);
+
+        return $result;
+    }
+
+    public function getByTopicId($topicId): GetListVocabulariesResult
+    {
+        $result = new GetListVocabulariesResult();
+        if (NumberHelper::isPositiveInteger($topicId) === false) {
+            $result->message = sprintf(ErrorMessage::INVALID_PARAMETER, 'topicId');
+            return $result;
+        }
+
+        $data = Database::select("SELECT id, name, pronunciation, meaning, image, topic_id FROM {$this->tableName} WHERE topic_id=$topicId");
+        $result->data = Vocabulary::getMapper()->mapFromDbToListEntities($data);
+
+        $result->success = ArrayHelper::isHasItems($result->data);
+        if ($result->success) {
+            $result->message = sprintf(SuccessMessage::FOUND_LIST_ITEMS, VocabularyConstants::NAME);
+        } else {
+            $result->message = sprintf(ErrorMessage::NOT_FOUND_ITEM, VocabularyConstants::NAME);
+            $result->responseCode = HttpResponseCode::NOT_FOUND;
+            $result->data = $data;
+        }
 
         return $result;
     }
